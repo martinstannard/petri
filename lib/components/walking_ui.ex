f43f6d71180af10af47e01efe038be38384f410d
Martin Stannard <martin@stannard.id.au>
2018-10-16 00:10:16 +1100

    add walking ui

---
+++ lib/components/walking_ui.ex
@@ -0,0 +1,48 @@
+defmodule Processor.Component.WalkingUI do
+  use Scenic.Component
+
+  alias Scenic.ViewPort
+  alias Scenic.Graph
+
+  import Scenic.Primitives, only: [{:text, 3}]
+  import Scenic.Components
+
+  @height 110
+  @font_size 20
+
+  # --------------------------------------------------------
+  def verify(scene) when is_atom(scene), do: {:ok, scene}
+  def verify({scene, _} = data) when is_atom(scene), do: {:ok, data}
+  def verify(_), do: :invalid_data
+
+  # ----------------------------------------------------------------------------
+  def init(current_scene, opts) do
+    # Get the viewport width
+    {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} =
+      opts[:viewport]
+      |> ViewPort.info()
+
+    graph =
+      Graph.build(font_size: @font_size, translate: {0, vp_height - @height})
+      # |> text("Run", t: {vp_width - 330, @font_size * 3})
+      # |> toggle(true, id: :run, t: {vp_width - 270, @font_size * 3})
+      |> button("Start",
+        id: :run,
+        theme: :secondary,
+        translate: {vp_width - 500, @font_size * 2}
+      )
+      |> button("Add 1",
+        id: :btn_one,
+        theme: :secondary,
+        translate: {vp_width - 200, @font_size * 2}
+      )
+      |> button("Add 10",
+        id: :btn_ten,
+        theme: :secondary,
+        translate: {vp_width - 100, @font_size * 2}
+      )
+      |> push_graph()
+
+    {:ok, %{graph: graph, viewport: opts[:viewport]}}
+  end
+end
