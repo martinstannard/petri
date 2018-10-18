1a00c5566744ef8ae55131b26db270ee5ecc524e
Martin Stannard <martin@stannard.id.au>
2018-10-18 22:21:03 +1100

    add Modular

---
+++ lib/arena/utils/modular.ex
@@ -0,0 +1,26 @@
+defmodule Utils.Modular do
+  @moduledoc """
+  helper functions for modules used in scenes and creatures
+  """
+
+  def init_modules(state, modules) do
+    modules
+    |> Enum.reduce(state, fn m, s ->
+      s |> m.init
+    end)
+  end
+
+  def call_modules(state, modules, world) do
+    modules
+    |> Enum.reduce(state, fn m, s ->
+      s |> m.call(world)
+    end)
+  end
+
+  def call_modules(state, modules) do
+    modules
+    |> Enum.reduce(state, fn m, s ->
+      s |> m.call
+    end)
+  end
+end
