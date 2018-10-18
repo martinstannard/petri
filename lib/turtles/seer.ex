9d39f901e7bb186a3e2cf2b5f0f1cc556a39d501
Martin Stannard <martin@stannard.id.au>
2018-10-18 22:20:24 +1100

    update creatures

--- lib/turtles/seer.ex
+++ lib/turtles/seer.ex
@@ -4,23 +4,15 @@ defmodule Processor.Turtles.Seer do
   """
   use GenServer
 
-  alias Scenic.Graph
-
-  alias Processor.Turtles.Utils
-
-  alias Processor.Turtles.Behaviour.{
-    Colorize,
-    Feed,
-    Scale,
-    Smell,
-    Vision,
-    Wiggle
-  }
-
+  import Utils.Modular
   import Scenic.Primitives
 
-  @max_health 1000
+  alias Scenic.Graph
+  alias Processor.Turtles.Utils
+  alias Processor.Turtles.Behaviour.{Feed, Health, Move, Vision}
+
   @tri {{0, -15}, {8, 8}, {-8, 8}}
+  @modules [Health, Feed, Vision, Move]
 
   def start_link(id) do
     GenServer.start_link(__MODULE__, id)
@@ -51,40 +43,15 @@ defmodule Processor.Turtles.Seer do
   end
 
   def init(id) do
-    state = %{
-      id: id,
-      heading: Enum.random(0..628) / 100.0,
-      angle: Enum.random(20..100) * Enum.random([-1.0, 1.0]),
-      velocity: Enum.random(5..20) / 20.0,
-      x: Enum.random(0..800),
-      y: Enum.random(0..800),
-      color: Utils.random_color(),
-      scale: Enum.random(0..2000) / 1000.0 + 1.0,
-      health: @max_health,
-      tick: 0
-    }
-
-    new_state =
-      state
-      |> Vision.init()
-      |> Feed.init(@max_health)
-
     {
       :ok,
-      new_state
+      %{id: id}
+      |> init_modules(@modules)
     }
   end
 
   def handle_cast({:update, world}, state) do
-    new_state =
-      state
-      |> tick
-      |> Vision.call(world)
-      |> Wiggle.call()
-      |> Feed.call()
-      |> move()
-
-    {:noreply, new_state}
+    {:noreply, state |> call_modules(@modules, world)}
   end
 
   def handle_call({:draw, graph}, _, state) do
@@ -114,7 +81,7 @@ defmodule Processor.Turtles.Seer do
       &update_opts(&1,
         translate: {state.x, state.y},
         rotate: state.heading,
-        fill: {:color, health_colour(state.health)}
+        fill: {:color, state.color}
       )
     )
   end
@@ -123,71 +90,5 @@ defmodule Processor.Turtles.Seer do
     graph
     |> triangle(@tri, id: state.id)
     |> text("test", id: "#{state.id}_text", fill: {:color, :blue})
-  end
-
-  def tick(state) do
-    %{state | tick: state.tick + 1, health: state.health - 1}
-  end
-
-  def move(state) do
-    state
-    |> forward
-
-    # |> turn
-  end
-
-  def turn(state) do
-    #   %{state | heading: state.heading + 2.0 * :math.pi()}
-    #   state
-    #   |> Map.put(:heading, state.heading - state.angle)
-  end
-
-  # def turn(%{food_delta: fd} = state) when fd < 0 do
-  #   state
-  # end
-
-  def turn(state) do
-    state
-    |> Map.put(:heading, state.heading - state.angle)
-    |> clamp_heading
-  end
-
-  def clamp_heading(%{heading: heading} = state) when heading > 6.283185307179586 do
-    %{state | heading: state.heading - 2.0 * :math.pi()}
-  end
-
-  def clamp_heading(%{heading: heading} = state) when heading < 0 do
-    %{state | heading: state.heading + 2.0 * :math.pi()}
-  end
-
-  def clamp_heading(state), do: state
-
-  def forward(state) do
-    %{
-      state
-      | x: new_x(state.x, state.heading, state.velocity),
-        y: new_y(state.y, state.heading, state.velocity)
-    }
-  end
-
-  defp new_x(x, heading, distance) do
-    new_x = x + :math.sin(heading) * distance
-    bound(new_x)
-  end
-
-  defp new_y(y, heading, distance) do
-    new_y = y - :math.cos(heading) * distance
-    bound(new_y)
-  end
-
-  defp bound(coord) when coord > 800.0, do: coord - 800.0
-  defp bound(coord) when coord < 0.0, do: coord + 800.0
-  defp bound(coord), do: coord
-
-  defp health_colour(health) do
-    percentage = health / @max_health
-    r = round(255.0 * (1.0 - percentage))
-    g = round(255.0 * percentage)
-    {r, g, 0x22}
   end
 end
