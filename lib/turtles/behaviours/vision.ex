0e6bb659d4afc443084d7d26638af900a02c8379
Martin Stannard <martin@stannard.id.au>
2018-10-22 22:23:59 +1100

    got Vision working, with some tests

--- lib/turtles/behaviours/vision.ex
+++ lib/turtles/behaviours/vision.ex
@@ -2,24 +2,70 @@ defmodule Processor.Turtles.Behaviour.Vision do
   @moduledoc """
   models vision
   """
+  @twopi 2.0 * :math.pi()
 
   def init(state) do
-    state
-    |> Map.put(:eye_width, 1.0)
-    |> Map.put(:eye_offset, 60.0)
-    |> Map.put(:bearing, 4.0)
+    ns =
+      state
+      |> Map.put(:eye_width, :rand.uniform() * :math.pi())
+      |> Map.put(:eye_offset, :rand.uniform() * :math.pi())
   end
 
   def call(state, world) do
+    %{state | heading: state.heading + angle(state, world)}
+  end
+
+  def angle(state, world) do
     bearing = bearing(world.food_x, world.food_y, state)
-
-    %{state | heading: bearing + state.angle, bearing: bearing}
+    turn(left_eye_sees(bearing, state), right_eye_sees(bearing, state), state.angle)
   end
 
-  defp bearing(px, py, state) do
-    y = state.x - px
-    x = py - state.y
+  def turn(true, true, _angle), do: 0.0
+  def turn(false, true, angle), do: angle
+  def turn(true, false, angle), do: angle * -1.0
+  def turn(false, false, angle), do: angle
 
-    :math.atan2(y, x) + :math.pi()
+  def left_eye_sees(bearing, state) do
+    rs = (@twopi - state.eye_offset + state.eye_width / 2.0) |> clamp
+    ls = (@twopi - state.eye_offset - state.eye_width / 2.0) |> clamp
+
+    eye_sees(ls, rs, bearing)
   end
+
+  def right_eye_sees(bearing, state) do
+    rs = (state.eye_offset + state.eye_width / 2.0) |> clamp
+    ls = (state.eye_offset - state.eye_width / 2.0) |> clamp
+
+    eye_sees(ls, rs, bearing)
+  end
+
+  def eye_sees(ls, rs, bearing) when ls < rs do
+    cond do
+      bearing < rs and bearing > ls ->
+        true
+
+      true ->
+        false
+    end
+  end
+
+  def eye_sees(ls, rs, bearing) do
+    cond do
+      bearing < rs or bearing > ls ->
+        true
+
+      true ->
+        false
+    end
+  end
+
+  def bearing(px, py, state) do
+    a = :math.atan2(state.x - px, py - state.y) + :math.pi()
+
+    (state.heading - a) |> clamp
+  end
+
+  defp clamp(angle) when angle > @twopi, do: angle - @twopi
+  defp clamp(angle) when angle < 0, do: angle + @twopi
+  defp clamp(angle), do: angle
 end
---
+++ test/turtles/behaviours/vision_test.exs
@@ -0,0 +1,152 @@
+defmodule Processor.Turtles.Behaviour.VisionTest do
+  use ExUnit.Case, async: true
+
+  alias Processor.Turtles.Behaviour.Vision
+
+  @pi :math.pi()
+  @twopi :math.pi() * 2.0
+  @piontwo :math.pi() / 2.0
+  @threepiontwo :math.pi() * 3.0 / 2.0
+
+  describe "left eye not overlapping 0" do
+    setup do
+      %{
+        eye_offset: 1.0,
+        eye_width: 1.0
+      }
+    end
+
+    test "for different bearings", state do
+      assert(Vision.left_eye_sees(0.0, state) == false)
+      assert(Vision.left_eye_sees(3.0, state) == false)
+      assert(Vision.left_eye_sees(@twopi - 0.25, state) == false)
+      assert(Vision.left_eye_sees(@twopi - 0.51, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 0.99, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 1.49, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 1.51, state) == false)
+    end
+  end
+
+  describe "left eye overlapping 0" do
+    setup do
+      %{
+        eye_offset: 0.5,
+        eye_width: 2.0
+      }
+    end
+
+    test "multiple bearings", state do
+      assert(Vision.left_eye_sees(1.51, state) == false)
+      assert(Vision.left_eye_sees(0.0, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 0.25, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 0.51, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 0.99, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 1.49, state) == true)
+      assert(Vision.left_eye_sees(@twopi - 1.51, state) == false)
+    end
+  end
+
+  describe "right eye not overlapping" do
+    setup do
+      %{
+        eye_offset: 1.0,
+        eye_width: 1.0
+      }
+    end
+
+    test "multiple bearings", state do
+      assert(Vision.right_eye_sees(0.0, state) == false)
+      assert(Vision.right_eye_sees(3.0, state) == false)
+      assert(Vision.right_eye_sees(0.25, state) == false)
+      assert(Vision.right_eye_sees(0.51, state) == true)
+      assert(Vision.right_eye_sees(0.99, state) == true)
+      assert(Vision.right_eye_sees(1.49, state) == true)
+      assert(Vision.right_eye_sees(1.51, state) == false)
+    end
+  end
+
+  describe "right eye overlapping 0" do
+    setup do
+      %{
+        eye_offset: 0.5,
+        eye_width: 2.0
+      }
+    end
+
+    test "multiple bearings", state do
+      assert(Vision.right_eye_sees(@twopi - 0.51, state) == false)
+      assert(Vision.right_eye_sees(@twopi - 0.25, state) == true)
+      assert(Vision.right_eye_sees(0.0, state) == true)
+      assert(Vision.right_eye_sees(0.51, state) == true)
+      assert(Vision.right_eye_sees(0.99, state) == true)
+      assert(Vision.right_eye_sees(1.49, state) == true)
+      assert(Vision.right_eye_sees(1.51, state) == false)
+      assert(Vision.right_eye_sees(1.51, state) == false)
+    end
+  end
+
+  describe "turning" do
+    test "with both eyes true returns 0" do
+      assert(Vision.turn(true, true, 30.0) == 0.0)
+    end
+
+    test "with no eyes true returns angle" do
+      assert(Vision.turn(false, false, 30.0) == 30.0)
+    end
+
+    test "with left eye true returns -angle" do
+      assert(Vision.turn(true, false, 30.0) == -30.0)
+    end
+
+    test "with right eye true returns angle" do
+      assert(Vision.turn(false, true, 30.0) == 30.0)
+    end
+  end
+
+  describe "eye_sees" do
+    test "is to the right of the right side" do
+      assert(Vision.eye_sees(1.0, 2.0, 3.0) == false)
+    end
+
+    test "is to the left of the left side" do
+      assert(Vision.eye_sees(1.0, 2.0, 0.1) == false)
+    end
+
+    test "is between the left and right sides" do
+      assert(Vision.eye_sees(1.0, 2.0, 1.1) == true)
+    end
+  end
+
+  describe "bearing" do
+    setup do
+      %{
+        x: 100.0,
+        y: 100.0,
+        heading: 0.0
+      }
+    end
+
+    test "heading 0", state do
+      assert(Vision.bearing(100.0, -100.0, state) == 0.0)
+      assert(Vision.bearing(100.0, 200.0, state) == @pi)
+      assert(Vision.bearing(200.0, 100.0, state) == @threepiontwo)
+      assert(Vision.bearing(-100.0, 100.0, state) == @piontwo)
+    end
+
+    test "heading pi", state do
+      state = Map.put(state, :heading, @pi)
+      assert(Vision.bearing(100.0, -100.0, state) == @pi)
+      assert(Vision.bearing(100.0, 200.0, state) == 0.0)
+      assert(Vision.bearing(200.0, 100.0, state) == @piontwo)
+      assert(Vision.bearing(-100.0, 100.0, state) == @threepiontwo)
+    end
+
+    test "heading pi / 2.0", state do
+      state = Map.put(state, :heading, @piontwo)
+      assert(Vision.bearing(100.0, -100.0, state) == @piontwo)
+      assert(Vision.bearing(100.0, 200.0, state) == @threepiontwo)
+      assert(Vision.bearing(200.0, 100.0, state) == 0.0)
+      assert(Vision.bearing(-100.0, 100.0, state) == @pi)
+    end
+  end
+end
