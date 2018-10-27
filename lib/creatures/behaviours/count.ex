7e7b1f03b241367eb5feb14bcd2fce3ac8f8a48d
Martin Stannard <martin@stannard.id.au>
2018-10-27 14:42:35 +1100

    add creature Count behaviour

---
+++ lib/creatures/behaviours/count.ex
@@ -0,0 +1,15 @@
+defmodule Processor.Creatures.Behaviour.Count do
+  @moduledoc """
+  adds a count member to a creature's state
+  """
+
+  def init(state, _opts \\ %{}) do
+    state
+    |> Map.put(:count, 0)
+  end
+
+  def call(state) do
+    state
+    |> Map.put(:count, state.count + 1)
+  end
+end
